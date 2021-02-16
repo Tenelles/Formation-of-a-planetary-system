6290880ea5d7bb19f142df369b83dbf52269d6b5
tenelles <tenelles@yandex.ru>
2021-02-16 21:12:05 +0300

    fixed projection methods in vector and added to circle

--- Formation_of_a_planetary_system.py
+++ Formation_of_a_planetary_system.py
@@ -10,18 +10,27 @@ class Vector3:
     def list(self):
         return [self.x, self.y, self.z]
     
-    def drawYZ(self):
-        gr.Point(y, z)
+    def projectionYZ(self):
+        return gr.Point(self.y, self.z)
 
-    def drawXZ(self):
-        gr.Point(x, z)
+    def projectionXZ(self):
+        return gr.Point(self.x, self.z)
 
-    def drawXY(self):
-        gr.Point(x, y)
+    def projectionXY(self):
+        return gr.Point(self.x, self.y)
 
 class Sphere:
 
     def __init__(self, center, radius):
         self.center = center
         self.radius = radius
+
+    def projectionYZ(self):
+        return gr.Circle(self.center.projectionYZ(), self.radius)
+        
+    def projectionXZ(self):
+        return gr.Circle(self.center.projectionXZ(), self.radius)
+        
+    def projectionXY(self):
+        return gr.Circle(self.center.projectionXY(), self.radius)
 
