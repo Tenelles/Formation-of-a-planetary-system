c270159497d4ab8388e9286a555d1d5fab8910d0
tenelles <tenelles@yandex.ru>
2021-02-16 20:07:04 +0300

    Added Vector3 class

--- Formation_of_a_planetary_system.py
+++ Formation_of_a_planetary_system.py
@@ -1,2 +1,11 @@
 import graphics as gr
 
+class Vector3:
+
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def list(self):
+        return [self.x, self.y, self.z]
